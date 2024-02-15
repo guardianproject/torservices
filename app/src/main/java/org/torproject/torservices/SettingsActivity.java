9006c6ae20afe4a77fcbcd3dc9e7ca61d2db31ce
Michael Pöhn <michael@poehn.at>
2024-02-15 15:30:36 +0100

    🐑 switch to appcompat

--- app/src/main/java/org/torproject/torservices/SettingsActivity.java
+++ app/src/main/java/org/torproject/torservices/SettingsActivity.java
@@ -1,47 +1,47 @@
 package org.torproject.torservices;
 
-import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.preference.SwitchPreference;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
 
 import org.torproject.jni.TorService;
 
-import androidx.core.app.NotificationManagerCompat;
-
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
 
     static final String PREF_ABOUT = "pref_about";
     static final String PREF_USE_PERSISTENT_NOTIFICATIONS = "pref_use_persistent_notifications";
     static final String PREF_ALLOW_BACKGROUND_STARTS = "pref_allow_background_starts";
     static final String PREF_START_ON_BOOT = "pref_start_on_boot";
 
-    public static class MySettingsFragment extends PreferenceFragment {
+    public static class MySettingsFragment extends PreferenceFragmentCompat {
+
+        BroadcastReceiver broadcastReceiver;
+
         @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+        public void onCreatePreferences(@Nullable Bundle bundle, @Nullable String s) {
             addPreferencesFromResource(R.xml.preferences);
 
             String about = String.format("v%s with tor v%s", BuildConfig.VERSION_NAME, TorService.VERSION_NAME);
             findPreference(PREF_ABOUT).setTitle(about);
 
-            SwitchPreference usePersistentNotification = (SwitchPreference) findPreference(
+            SwitchPreference usePersistentNotification = findPreference(
                     PREF_USE_PERSISTENT_NOTIFICATIONS);
-            usePersistentNotification.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object enabled) {
-                    // TODO put up/take down notification, e.g. set foreground
-                    if (Build.VERSION.SDK_INT < 26) {
-                        if ((Boolean) enabled) {
-                            App.startTorServiceForeground(getActivity());
-                        } else {
-                            NotificationManagerCompat.from(getActivity()).cancelAll();
-                        }
+            usePersistentNotification.setOnPreferenceChangeListener((preference, enabled) -> {
+                // TODO put up/take down notification, e.g. set foreground
+                if (Build.VERSION.SDK_INT < 26) {
+                    if ((Boolean) enabled) {
+                        App.startTorServiceForeground(getActivity());
+                    } else {
+                        NotificationManagerCompat.from(getContext()).cancelAll();
                     }
-                    return true;
                 }
+                return true;
             });
             if (Build.VERSION.SDK_INT >= 26) {
                 usePersistentNotification.setChecked(true);
@@ -53,7 +53,7 @@ public class SettingsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager()
+        getSupportFragmentManager()
                 .beginTransaction()
                 .replace(android.R.id.content, new MySettingsFragment())
                 .commit();
